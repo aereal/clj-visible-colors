560683af82127eeb390bf8c3adc02757f72908ce
aereal <aereal@aereal.org>
2013-12-23 03:01:31 +0900

    Check the brightness or heu difference of the colors should be visible

--- src/clj_visible_colors/core.clj
+++ src/clj_visible_colors/core.clj
@@ -1,6 +1,34 @@
-(ns clj-visible-colors.core)
+(ns clj-visible-colors.core
+  [:require [com.github.bdesham.clj-plist :refer :all]])
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defrecord Color [red green blue])
+
+(defn make-color-object-from-values
+  [rgb]
+  (apply #(->Color %1 %2 %3) rgb))
+
+(defn parse-color-scheme
+  [f]
+  (let [plist (parse-plist (java.io.File. f))]
+    (map #(make-color-object-from-values (vals %)) (vals plist))))
+
+(defn brighness-of-component
+  [c v]
+  (let [k {:red 299 :green 587 :blue 114}]
+    (* 255 v (c k))))
+
+(defn brightness
+  [c]
+  (reduce + (map #(brighness-of-component (key %) (val %)) c)))
+
+(defn has-visible-brightness-contrast
+  [ca cb]
+  <= 125 (. Math abs (reduce - (map #(brightness %) (list ca cb)))))
+
+(defn hue-contrast
+  [ca cb]
+  (* 255 (reduce + (map #(- (max (% ca) (% cb)) (min (% ca) (% cb))) (list :red :green :blue)))))
+
+(defn has-visible-hue-contrast
+  [ca cb]
+  (<= 500 (hue-contrast ca cb)))
--- test/clj_visible_colors/core_test.clj
+++ test/clj_visible_colors/core_test.clj
@@ -2,6 +2,12 @@
   (:require [clojure.test :refer :all]
             [clj-visible-colors.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(let [black (->Color 0.0 0.0 0.0) white (->Color 1.0 1.0 1.0)]
+  (deftest has-visible-brightness-contrast-test
+    (testing "pure black and pure white have visible brightness"
+      (is (has-visible-brightness-contrast black white))))
+
+  (deftest has-visible-hue-contrast-test
+    (testing "pure black and pure white have visible hue contrast"
+      (is (has-visible-hue-contrast black white))))
+  )
